b3a4424642a9806973cb60ef7f80fa0d3b2fc930
JavaDeveloper <Kozhevnikov95igor@gmail.com>
2022-08-23 03:01:16 +0300

    hash&equals

---
+++ src/Author.java
@@ -0,0 +1,36 @@
+import java.util.Objects;
+
+public class Author {
+    private final String name;
+    private final String lastname;
+
+    public Author(String name, String lastname) {
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String toString() {
+        return "Автор: " + name + " " + lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(lastname, author.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastname);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашнее задание");
+        // Задание 1
+        Author aPushkin = new Author("Александр", "Пушкин");
+        Author fDostoevskiy = new Author("Федор", "Достоевский");
+        Book evgeniyOnegin = new Book("Евгений Онегин", 1833, aPushkin);
+        Book crimeAndPunishment = new Book("Преступление и наказание", 1866, fDostoevskiy);
+
+
+        System.out.println(aPushkin);
+        System.out.println(fDostoevskiy);
+        System.out.println(evgeniyOnegin);
+        System.out.println(crimeAndPunishment);
+
+        crimeAndPunishment.setPublishDate(2022);
+        System.out.println();
+        System.out.println(crimeAndPunishment);
+        System.out.println("Равны ли книги? - " + evgeniyOnegin.equals(crimeAndPunishment));
+        System.out.println("Hashcode - " + evgeniyOnegin.hashCode());
     }
 }
